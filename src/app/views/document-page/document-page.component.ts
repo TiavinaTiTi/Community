501d9a1e74222e64a00984a02af39f962785205e
TiavinaTiTi <titus50231@gmail.com>
2024-05-30 13:39:26 +0300

    Creation template docs-page

--- src/app/views/document-page/document-page.component.ts
+++ src/app/views/document-page/document-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-document-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './document-page.component.html',
   styleUrl: './document-page.component.scss'
 })
 export class DocumentPageComponent {
+  search: string = '';
 
+  searchTraining() {
+
+  }
 }
