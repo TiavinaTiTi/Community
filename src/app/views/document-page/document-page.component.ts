c0aae1df6b1f680e4c4088caf9eedbf888f4db64
TiavinaTiTi <titus50231@gmail.com>
2024-06-03 09:41:34 +0300

    Creation loader lors du chargement components docs

--- src/app/views/document-page/document-page.component.ts
+++ src/app/views/document-page/document-page.component.ts
@@ -1,10 +1,10 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {ItemDocumentComponent} from "../item-document/item-document.component";
 import {DocumentService} from "../../shared/services/documentService/document.service";
 import {Observable} from "rxjs";
 import {DocumentModel} from "../../core/models/document.model";
-import {AsyncPipe} from "@angular/common";
+import {AsyncPipe, DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-document-page',
@@ -13,18 +13,30 @@ import {AsyncPipe} from "@angular/common";
     ReactiveFormsModule,
     FormsModule,
     ItemDocumentComponent,
-    AsyncPipe
+    AsyncPipe,
+    DatePipe
   ],
   templateUrl: './document-page.component.html',
   styleUrl: './document-page.component.scss'
 })
-export class DocumentPageComponent {
+export class DocumentPageComponent implements OnInit{
   search: string = '';
 
   documentService: DocumentService = inject(DocumentService)
 
-  documents: Observable<DocumentModel[]> = this.documentService.getAllDocuments()
-  searchTraining() {
+  documents!: Observable<DocumentModel[]>;
+  loaderDocs: number[] = [1,2,3,4,5,6]
 
+  ngOnInit() {
+    this.chargeData()
   }
+
+  searchTraining() {}
+
+  chargeData(){
+    setTimeout(()=>{
+      this.documents = this.documentService.getAllDocuments()
+    }, 2500)
+  }
+
 }
