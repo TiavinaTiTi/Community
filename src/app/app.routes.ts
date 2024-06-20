49e45d95bb1cd44ffede83269c274e810c76947c
TiavinaTiTi <titus50231@gmail.com>
2024-06-20 23:50:12 +0300

    Laizy loading dans root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from "./views/home-page/home-page.component";
-import {TrainingPageComponent} from "./views/training-page/training-page.component";
-import {IsRightComponent} from "./shared/test/is-right/is-right.component";
-import {IsLeftComponent} from "./shared/test/is-left/is-left.component";
-import {IsMeddleComponent} from "./shared/test/is-meddle/is-meddle.component";
 import {ContactPageComponent} from "./views/contact-page/contact-page.component";
-import {TeamPageComponent} from "./views/team-page/team-page.component";
 import {LoginPageComponent} from "./views/login-page/login-page.component";
 import {DocumentPageComponent} from "./views/document-page/document-page.component";
 import {SignUpPageComponent} from "./views/sign-up-page/sign-up-page.component";
@@ -18,12 +13,28 @@ export const routes: Routes = [
     loadComponent: ()=> import('./views/team-page/team-page.component').then(m => m.TeamPageComponent),
     data: { animation: 'team' }
   },
-  // { path: 'training', component: TrainingPageComponent, data: { animation: 'training' } },
-  { path: 'docs', component: DocumentPageComponent, data: {animation: 'docs'} },
-  { path: 'contact', component: ContactPageComponent, data: { animation: 'contact' } },
-  { path: 'login', component: LoginPageComponent, data: { animation: 'login' } },
-  { path: 'sign-up', component: SignUpPageComponent, data: { animation: 'sign-up' } },
-  /*{ path: 'isRight', component: IsRightComponent, data: { animation: 'isRight'} },
-  { path: 'isLeft', component: IsLeftComponent, data: { animation: 'isLeft' } },
-  { path: 'isMeddle', component: IsMeddleComponent, data: { animation: 'isMeddle' } },*/
+  {
+    path: 'docs',
+    // component: DocumentPageComponent,
+    loadComponent: ()=> import('./views/document-page/document-page.component').then(m => m.DocumentPageComponent),
+    data: {animation: 'docs'}
+  },
+  {
+    path: 'contact',
+    // component: ContactPageComponent,
+    loadComponent: ()=> import('./views/contact-page/contact-page.component').then(m => m.ContactPageComponent),
+    data: { animation: 'contact' }
+  },
+  {
+    path: 'login',
+    // component: LoginPageComponent,
+    loadComponent: ()=> import('./views/login-page/login-page.component').then(m => m.LoginPageComponent),
+    data: { animation: 'login' }
+  },
+  {
+    path: 'sign-up',
+    // component: SignUpPageComponent,
+    loadComponent: ()=> import('./views/sign-up-page/sign-up-page.component').then(m => m.SignUpPageComponent),
+    data: { animation: 'sign-up' }
+  }
 ];
